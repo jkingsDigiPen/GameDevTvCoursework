dafffd7bf9a4739bca6c44ccd1e3cf8d2fa6fdc8
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2021-11-30 14:58:30 -0800

    Update player movement to match movement from 1st 2D project.

--- Unity3D/01 Obstacle Course/Assets/Scripts/PlayerMovementController.cs
+++ Unity3D/01 Obstacle Course/Assets/Scripts/PlayerMovementController.cs
@@ -1,18 +1,55 @@
+// Name: PlayerMovementController.cs
+// Author(s): Jeremy kings
+// Purpose: Move and rotate player
+// avatar based on user input.
+//
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float rotationSpeed = 90.0f;
+    [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] float reverseSpeedMultiplier = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool isMoving = Move();
+
+        Rotate(isMoving);
+    }
+
+    private void Rotate(bool isMoving)
+    {
+        // Disallow rotation when car is
+        // not already moving
+        if (!isMoving)
+            return;
+
+        // Make sure rotation works opposite way when reversing
+        // to mimic car controls
+        float sign = Mathf.Sign(Input.GetAxis("Vertical"));
+
+        float speedMod = rotationSpeed * (sign < 0.0f ? reverseSpeedMultiplier : 1.0f);
+
+        float direction = Input.GetAxis("Horizontal") * sign;
+
+        transform.Rotate(0, direction * speedMod * Time.deltaTime, 0);
+    }
+
+    private bool Move()
+    {
+        Vector3 direction = Vector3.forward;
+
+        float sign = Input.GetAxis("Vertical");
+
+        float speedMod = moveSpeed * (sign < 0.0f ? reverseSpeedMultiplier : 1.0f);
+
+        transform.Translate(direction * sign * speedMod * Time.deltaTime);
+
+        return sign != 0.0f;
     }
 }
